06601d60746010183453cc714d00f5f8d179e105
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-24 18:39:21 +0300

    updated the toppings section

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#final-order-details").hide();
     $("#deliver").hide();
+    //business logic
+    var totalPriceArray = [];
+
+    function Order(size, crust, toppings, amount) {
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.pizzaPrice = 0;
+        this.amount = amount;
+
+    }
+    Order.prototype.pizzaCost = function () {
+        if (this.size === "small-pizza") {
+            this.pizzaPrice += 550;
+        } else if (this.size === "medium-pizza") {
+            this.pizzaPrice += 800;
+        } else if (this.size === "large-pizza") {
+            this.pizzaPrice += 1200;
+        }
+        if (this.crust === "cheese-filled") {
+            this.pizzaPrice += 150;
+        } else if (this.crust === "gluten-free") {
+            this.pizzaPrice += 185;
+        } else if (this.crust === "stuffed") {
+            this.pizzaPrice += 170;
+        } else if (this.crust === "crispy") {
+            this.pizzaPrice += 150;
+        }
+        if (this.toppings === "pepperoni") {
+            this.pizzaPrice += 100;
+        } else if (this.toppings === "olives") {
+            this.pizzaPrice += 150;
+        } else if (this.toppings === "bacon") {
+            this.pizzaPrice += 250;
+        } else if (this.toppings === "mushrooms") {
+            this.pizzaPrice += 150;
+        } else if (this.toppings === "chicken-bbq") {
+            this.pizzaPrice += 250;
+        } else if (this.toppings === "pinapple") {
+            this.pizzaPrice += 105;
+        } else if (this.toppings === "cheese") {
+            this.pizzaPrice += 165;
+        }
+    };
 });
